b8e14e3a55164838488318af69c377dd1aa437cd
Tom Wardill <tom@howrandom.net>
2011-03-10 12:58:09 +0000

    Fix including the readme in the egg

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='twfablibrary',
-    version='0.0.4',
+    version='0.0.5',
     packages=find_packages(exclude=['ez_setup']),
     zip_safe=False,
     include_package_data=True,
