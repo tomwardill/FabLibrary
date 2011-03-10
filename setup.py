c1d703f16aea851b1d8f47143c440dd966349e0c
Tom Wardill <tom@howrandom.net>
2011-03-10 12:49:56 +0000

    Added classifiers and filled out the setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,22 @@ setup(
     packages=find_packages(exclude=['ez_setup']),
     zip_safe=False,
     include_package_data=True,
-    install_requires = ['fabric']
+    install_requires = ['fabric'],
+    description = 'Debian specific helper functions for fabric',
+    author = 'Tom Wardill',
+    author_email = 'tom@howrandom.net',
+    url = 'https://github.com/tomwardill/FabLibrary',
+    long_description = open('README.rst', 'r').read(),
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: System :: Installation/Setup',
+
+    ]
 )
 
